54b8b1eb058b73f8e94fd41a0965dfce2c75386d
Finns841594 <fengy142857@gmail.com>
2023-05-15 21:28:32 +0200

    modified data

--- database.ts
+++ database.ts
@@ -3,4 +3,7 @@ export const db = [
   {id: 2, breed: 'Labrador', name: 'Rocky', birthdate: '2019-01-07', },
   {id: 3, breed: 'Beagle', name: 'Buster', birthdate: '2019-01-07', },
   {id: 4, breed: 'Bulldog', name: 'Spike', birthdate: '2019-01-07', },
+  {id: 6, breed: 'Husky', name: 'Max', birthdate: '2019-01-07', },
+  {id: 7, breed: 'Poodle', name: 'Sam', birthdate: '2019-01-07', },
+  {id: 8, breed: 'Alaskan Malamute', name: 'Rocky', birthdate: '2019-01-07', },
 ]
